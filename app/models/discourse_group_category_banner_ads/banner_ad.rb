2a51e80084d528cb7462037d13ae4e86c13cd3dc
Gabriel Grubba <gabriel@discourse.org>
2024-10-24 16:55:00 -0300

    DEV: Update plugin annotations

--- app/models/discourse_group_category_banner_ads/banner_ad.rb
+++ app/models/discourse_group_category_banner_ads/banner_ad.rb
@@ -3,3 +3,19 @@
 class DiscourseGroupCategoryBannerAds::BannerAd < ::ActiveRecord::Base
   self.table_name = "banner_ads"
 end
+
+# == Schema Information
+#
+# Table name: banner_ads
+#
+#  id           :bigint           not null, primary key
+#  title        :string           default(""), not null
+#  cta_url      :string(2048)
+#  cta_text     :string           default("")
+#  banner_text  :string           default(""), not null
+#  category_ids :integer          default([]), is an Array
+#  group_ids    :integer          default([]), is an Array
+#  enabled      :boolean          default(FALSE), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
