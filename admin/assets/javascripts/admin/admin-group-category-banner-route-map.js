e3c5de23c8173c8bcad7b7a1f16cc22a966c2845
Jarek Radosz <jarek@cvx.dev>
2026-03-12 19:56:06 +0100

    boo

--- admin/assets/javascripts/admin/admin-group-category-banner-route-map.js
+++ admin/assets/javascripts/admin/admin-group-category-banner-route-map.js
@@ -1,7 +1,11 @@
+/* eslint-disable ember/route-path-style */
+
 export default {
   resource: "admin.adminPlugins",
   path: "/plugins",
   map() {
-    this.route("group-category-banner-ads");
+    this.route("group-category-banner-ads", {
+      path: "group_category_banner_ads",
+    });
   },
 };
